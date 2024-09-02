820f587f4ee963da4aeb92a4d588cd6c46a39814
Kinchula <matiksec23@proton.me>
2024-09-02 14:38:48 -0300

    Finalización parse_pipeline()

--- parsing.c
+++ parsing.c
@@ -53,23 +53,39 @@ pipeline parse_pipeline(Parser p) {
     cmd = parse_scommand(p);
     error = (cmd==NULL); /* Comando inválido al empezar */
     while (another_pipe && !error) {
-        /*
-         * COMPLETAR
-         *
-         */
+        //Si el parsr es nulo devielvo un mensaje de error y luego lo destruyo
+        if (cmd == NULL) {
+            printf("[ERROR]: No existe archivo de entrada o salida!\n"
+                              "Asegurece de ingresar un archivo valido.\n");
+
+            pipeline_destroy(result);
+            return NULL;
+        }
+        //Salteo los espacios en blaco en que caso de que los haya
+        parser_skip_blanks(p);
+        //Añado el comando simple al pipeline
+        pipeline_push_back(result, cmd);
+        //Busco un nuevo pipe. Si lo hay, lo consumo.
+        parser_op_pipe(p, &another_pipe);
     }
-    /* Opcionalmente un OP_BACKGROUND al final */
-    }
-    /*
-     *
-     * COMPLETAR
-     *
-     */
+
 
     /* Tolerancia a espacios posteriores */
     /* Consumir todo lo que hay inclusive el \n */
     /* Si hubo error, hacemos cleanup */
-
-    return NULL; // MODIFICAR
+    bool p_garbage; //Determinara si se encontraron símbolos distintos de espacios en blanco en el procesamiento
+    parser_garbage(p, &p_garbage);
+    //Si se encuentran caracteres inválidos despues del último comando de la secuencia
+    if (p_garbage) {
+        printf("ERROR: El comando ingresado es inválido.\n");
+        pipeline_destroy(result);
+        return NULL;
+    }
+    //Si al longitud del pipeline es 1, pero la longitud del primer comando simple es cero. Tambien puede ser un comando simple sin argumentos(inválido).
+    if ((pipeline_length(result) == 1) && (scommand_length(pipeline_front(result))==0)) {
+        printf("ERROR: No se ha ingresado un comando válido ó el comando ingresado no contiene argumentos o.\n");
+        pipeline_destroy(result);
+        return NULL;
+    }
+    return result;
 }
-

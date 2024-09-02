7ee29f95e15bc2218c7eefb27846fc28be00a487
José <jose.beretta@mi.unc.edu.ar>
2024-09-02 16:56:41 -0300

    builtin_alone y builtin_run

--- git pull/builtin.c
+++ git pull/builtin.c
@@ -13,8 +13,8 @@
 
 #define PATH_MAX 4096
 
-const unsigned int INTERNAL_COMMANDS_SIZE = 4u;
-const char *INTERNAL_COMMANDS[INTERNAL_COMMANDS_SIZE] = {"cd", "help", "exit", "pwd"};
+const unsigned int INTERNAL_COMMANDS_SIZE = 3u;
+const char *INTERNAL_COMMANDS[INTERNAL_COMMANDS_SIZE] = {"cd", "help", "exit"};
 
 bool builtin_is_internal(scommand cmd) {
     assert(cmd != NULL);
@@ -34,7 +34,9 @@ bool builtin_alone(pipeline p) {
 }
 
 static void builtin_cd(scommand cmd) {
-
+    char *path = NULL;
+    bool logical = true;
+    char *arg = NULL;
     scommand_pop_front(cmd);
     if (scommand_is_empty(cmd)) {
         chdir(getenv("HOME"));
@@ -44,7 +46,26 @@ static void builtin_cd(scommand cmd) {
             printf("%s\n", strerror(errno));
         }
     }
-}
+    while (!scommand_is_empty(cmd)) {
+        arg = scommand_front(cmd);
+        if (strcmp(arg, "-L") == 0) {
+            continue;
+        } else if (strcmp(arg, "-P") == 0) {
+            logical = false;
+    }
+    if (!logical) {  
+        char real_path[PATH_MAX];
+        if (realpath(path, real_path) != NULL) {
+            path = real_path;
+        } else {
+            printf("%s\n", strerror(errno));
+            return;
+        }
+    }
+    scommand_pop_front(cmd);
+    }
+} 
+
 
 static void builtin_pwd() {
     char cwd_buffer[PATH_MAX];
@@ -57,14 +78,14 @@ static void builtin_pwd() {
     // TODO: Redireccionar la salida al siguiente comando en el pipeline
 }
 
-
-
 void builtin_run(scommand cmd) {
     assert(builtin_is_internal(cmd));
     char *command = scommand_front(cmd);
+
     if (!strcmp(command, "cd")) { //strcmp es 0 cuando s1==s2
         builtin_cd(cmd);
     }
+
     if (strcmp(command, "help")) {
         printf("MM bash versión 1.0, creada por los estudiantes:\n"
         "Carlos Patrón \n"
@@ -80,9 +101,11 @@ void builtin_run(scommand cmd) {
         "bg -> Mueve el proceso a segundo plano.\n"
         );
     }
+
     if (strcmp(command, "exit")) {
         EXIT_SUCCESS; //To Do: Una vez creada bg, modificar esta función.
     }
+
     if (!strcmp(command, "pwd")) {
         builtin_pwd(cmd);
     }

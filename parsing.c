87481fd14d0ba4f3fba0e8dad75e953e8ec810c3
José <jose.beretta@mi.unc.edu.ar>
2024-09-08 01:46:57 -0300

    debug función parse_pipeline

--- parsing.c
+++ parsing.c
@@ -12,83 +12,75 @@ static scommand parse_scommand(Parser p) {
     bool error = false;
 
      while (!error && !parser_at_eof(p)) {
-        //parser_skip_blanks(p);
+        
         arg = parser_next_argument(p, &arg_type);
         if (arg == NULL) { 
             break;
         }
-        if (arg_type == ARG_INPUT) { //Comprueba si el arg es redir de entrada
-            if (scommand_get_redir_in(cmd) != NULL) {//Comprueba si ya había redirección de entrada
+        if (arg_type == ARG_INPUT) { //Comprueba si el arg es redir de entrada.
+            if (scommand_get_redir_in(cmd) != NULL) {//Comprueba si ya había redirección de entrada.
                 error = true;  
             } else {
                 scommand_set_redir_in(cmd, arg); //Si no dio error, setea la redirección de entrada.
             }
-        } else if (arg_type == ARG_OUTPUT) {//Comprueba si es redirección de salida
-            if (scommand_get_redir_out(cmd) != NULL) {//Comprueba si ya había redirección de salida
+        } else if (arg_type == ARG_OUTPUT) {//Comprueba si es redirección de salida.
+            if (scommand_get_redir_out(cmd) != NULL) {//Comprueba si ya había redirección de salida.
                 error = true;
             } else {
-                    scommand_set_redir_out(cmd, arg);//Setea si no hay redirección de salida previa
+                    scommand_set_redir_out(cmd, arg);//Setea si no hay redirección de salida previa.
                 }
         } else if (arg_type == ARG_NORMAL) {
-            scommand_push_back(cmd, arg);//agrego a cmd el arg normal
+            scommand_push_back(cmd, arg);//agrego a cmd el arg normal.
         }
+        parser_skip_blanks(p);
     }
-    if (error || scommand_is_empty(cmd)) {// Si ocurrió un error o cmd está vacío, limpiar y devolver NULL
+    if (error || scommand_is_empty(cmd)) {// Si ocurrió un error o cmd está vacío, limpiar y devolver NULL.
         scommand_destroy(cmd);
         cmd = NULL;
     }
     return cmd;
 }
 
+
+
 pipeline parse_pipeline(Parser p) {
-    assert ((p != NULL) && (!parser_at_eof (p)));
-    pipeline result = pipeline_new();
-    scommand cmd = NULL;
-    bool error = false, another_pipe=true;
-
-    cmd = parse_scommand(p);
-    if (!strcmp(scommand_front(cmd),"\n")) {
-        printf("comando vacio\n");
-    }
-    error = (cmd==NULL); /* Comando inválido al empezar */
-    while (another_pipe && !error) {
-        //Si el parsr es nulo devielvo un mensaje de error y luego lo destruyo
+    pipeline pipe = pipeline_new();
+    bool is_pipe = false;
+    bool has_op_background = false;
+    
+    while (!parser_at_eof(p)) {
+        scommand cmd = parse_scommand(p);
         if (cmd == NULL) {
-            printf("[ERROR]: No existe archivo de entrada o salida!\n"
-                              "Asegurece de ingresar un archivo valido.\n");
-
-            pipeline_destroy(result);
+            pipeline_destroy(pipe);
+            return NULL; 
+        }
+        pipeline_push_back(pipe, cmd);
+        parser_op_pipe(p, &is_pipe); //Esto verifica si hay un | en el comando, es decir si es una pipe.
+        if (is_pipe) {
+            continue; 
+        }
+        parser_op_background(p, &has_op_background); //Y esto verifica si hay un operador &, es decir de background.
+        if (has_op_background) {
+            pipeline_set_wait(pipe, false); //Si hay operador de background, no espera.
+        } else {
+            pipeline_set_wait(pipe, true); //Caso contrario, se espera.
+        }
+        parser_skip_blanks(p);
+        
+        bool garbage;
+        parser_garbage(p, &garbage);
+        if (garbage) {
+            char* garbage_str = parser_last_garbage(p);
+            if (garbage_str != NULL) {
+                printf("Basura encontrada: %s\n", garbage_str); 
+            }
+            pipeline_destroy(pipe); //Si se encuentra basura, puede ser un error, así que se destruye el pipe.
             return NULL;
         }
-        //Salteo los espacios en blaco en que caso de que los haya
-        parser_skip_blanks(p);
-        //Añado el comando simple al pipeline
-        pipeline_push_back(result, cmd);
-        //Busco un nuevo pipe. Si lo hay, lo consumo.
-        parser_op_pipe(p, &another_pipe);
-    }
 
-
-    /* Tolerancia a espacios posteriores */
-    /* Consumir todo lo que hay inclusive el \n */
-    /* Si hubo error, hacemos cleanup */
-    bool p_garbage; //Determinara si se encontraron símbolos distintos de espacios en blanco en el procesamiento
-    parser_garbage(p, &p_garbage);
-    //Si se encuentran caracteres inválidos despues del último comando de la secuencia
-    if (p_garbage) {
-        char *garbage_str = parser_last_garbage(p);
-        if (garbage_str != NULL) {
-                printf("Basura encontrada: %s\n", garbage_str); 
-        }
-        printf("ERROR: El comando ingresado es inválido.\n");
-        pipeline_destroy(result);
-        return NULL;
+        break; //Salir del bucle si no hay más comandos u operadores de pipe.
     }
-    //Si la longitud del pipeline es 1, pero la longitud del primer comando simple es cero. Tambien puede ser un comando simple sin argumentos(inválido).
-    if ((pipeline_length(result) == 1) && (scommand_length(pipeline_front(result))==0)) {
-        printf("ERROR: No se ha ingresado un comando válido ó el comando ingresado no contiene argumentos o.\n");
-        pipeline_destroy(result);
-        return NULL;
-    }
-    return result;
+    
+    return pipe;
 }
+
